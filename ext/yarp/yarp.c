a4d9dd13caf423a096acc8fe094e1a0aa3bbc5dc
Kevin Newton <kddnewton@gmail.com>
2022-09-13 11:06:31 -0400

    Correct the visibility on external functions for now

--- ext/yarp/yarp.c
+++ ext/yarp/yarp.c
@@ -1093,11 +1093,11 @@ parse_program(yp_parser_t *parser) {
 }
 
 /******************************************************************************/
-/* External functions                                                         */
+/* Functions that other libraries will eventually need                        */
 /******************************************************************************/
 
 // Initialize a parser with the given start and end pointers.
-void
+static void
 yp_parser_init(yp_parser_t *parser, const char *source, off_t size, yp_error_handler_t *error_handler) {
   *parser = (yp_parser_t) {
     .lex_modes = {
@@ -1114,14 +1114,14 @@ yp_parser_init(yp_parser_t *parser, const char *source, off_t size, yp_error_han
 }
 
 // Get the next token type and set its value on the current pointer.
-void
+static void
 yp_lex_token(yp_parser_t *parser) {
   parser->previous = parser->current;
   parser->current.type = lex_token_type(parser);
 }
 
 // Parse the Ruby source associated with the given parser and return the tree.
-yp_node_t *
+static yp_node_t *
 yp_parse(yp_parser_t *parser) {
   return parse_program(parser);
 }
--- ext/yarp/yarp.h
+++ ext/yarp/yarp.h
@@ -12,16 +12,4 @@
 #include <unistd.h>
 #include "nodes.h"
 
-// Initialize a parser with the given start and end pointers.
-void
-yp_parser_init(yp_parser_t *parser, const char *source, off_t size, yp_error_handler_t *error_handler);
-
-// Get the next token type and set its value on the current pointer.
-void
-yp_lex_token(yp_parser_t *parser);
-
-// Parse the Ruby source associated with the given parser and return the tree.
-yp_node_t *
-yp_parse(yp_parser_t *parser);
-
 #endif
