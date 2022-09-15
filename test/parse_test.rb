4e9e538296a1cfd313f4f365a2361a0ab65ed141
Kevin Newton <kddnewton@gmail.com>
2022-09-15 11:01:30 -0400

    Inline the test functions for more readability

--- test/parse_test.rb
+++ test/parse_test.rb
@@ -6,18 +6,58 @@ class ParseTest < Test::Unit::TestCase
   include YARP::DSL
 
   test "TERM < FACTOR" do
-    assert_equal higher_precedence(PLUS("+"), STAR("*")), YARP.parse("1 + 2 * 3")
+    actual = YARP.parse("1 + 2 * 3")
+    expected = Program(Statements([
+      Binary(
+        IntegerLiteral(INTEGER("1")),
+        PLUS("+"),
+        Binary(
+          IntegerLiteral(INTEGER("2")),
+          STAR("*"),
+          IntegerLiteral(INTEGER("3"))
+        )
+      )
+    ]))
+
+    assert_equal expected, actual
   end
 
   test "FACTOR < EXPONENT" do
-    assert_equal higher_precedence(STAR("*"), STAR_STAR("**")), YARP.parse("1 * 2 ** 3")
+    actual = YARP.parse("1 * 2 ** 3")
+    expected = Program(Statements([
+      Binary(
+        IntegerLiteral(INTEGER("1")),
+        STAR("*"),
+        Binary(
+          IntegerLiteral(INTEGER("2")),
+          STAR_STAR("**"),
+          IntegerLiteral(INTEGER("3"))
+        )
+      )
+    ]))
+
+    assert_equal expected, actual
   end
 
   test "FACTOR > TERM" do
-    assert_equal lower_precedence(STAR("*"), PLUS("+")), YARP.parse("1 * 2 + 3")
+    actual = YARP.parse("1 * 2 + 3")
+    expected = Program(Statements([
+      Binary(
+        Binary(
+          IntegerLiteral(INTEGER("1")),
+          STAR("*"),
+          IntegerLiteral(INTEGER("2"))
+        ),
+        PLUS("+"),
+        IntegerLiteral(INTEGER("3"))
+      )
+    ]))
+
+    assert_equal expected, actual
   end
 
   test "MODIFIER left associativity" do
+    actual = YARP.parse("a if b if c")
     expected = Program(
       Statements([
         IfModifier(
@@ -32,36 +72,6 @@ class ParseTest < Test::Unit::TestCase
       ])
     )
 
-    assert_equal expected, YARP.parse("a if b if c")
-  end
-
-  private
-
-  def lower_precedence(first_operator, second_operator)
-    Program(Statements([
-      Binary(
-        Binary(
-          IntegerLiteral(INTEGER("1")),
-          first_operator,
-          IntegerLiteral(INTEGER("2"))
-        ),
-        second_operator,
-        IntegerLiteral(INTEGER("3"))
-      )
-    ]))
-  end
-
-  def higher_precedence(first_operator, second_operator)
-    Program(Statements([
-      Binary(
-        IntegerLiteral(INTEGER("1")),
-        first_operator,
-        Binary(
-          IntegerLiteral(INTEGER("2")),
-          second_operator,
-          IntegerLiteral(INTEGER("3"))
-        )
-      )
-    ]))
+    assert_equal expected, actual
   end
 end
