d8f4b1396c9b88f63d7564a9cbfb74fe02e57b21
Kevin Newton <kddnewton@gmail.com>
2022-09-29 15:26:53 -0400

    Documentation on the buffer

--- ext/yarp/buffer.h
+++ ext/yarp/buffer.h
@@ -5,30 +5,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+// A yp_buffer_t is a simple memory buffer that stores data in a contiguous
+// block of memory. It is used to store the serialized representation of a
+// YARP tree.
 typedef struct {
   char *value;
   size_t length;
   size_t capacity;
 } yp_buffer_t;
 
+// Allocate a new yp_buffer_t with the default initial capacity.
 yp_buffer_t *
 yp_buffer_alloc();
 
+// Append a string to the buffer.
 void
 yp_buffer_append_str(yp_buffer_t *buffer, const char *value, size_t length);
 
+// Append a single byte to the buffer.
 void
 yp_buffer_append_u8(yp_buffer_t *buffer, uint8_t value);
 
+// Append a 16-bit unsigned integer to the buffer.
 void
 yp_buffer_append_u16(yp_buffer_t *buffer, uint16_t value);
 
+// Append a 32-bit unsigned integer to the buffer.
 void
 yp_buffer_append_u32(yp_buffer_t *buffer, uint32_t value);
 
+// Append a 64-bit unsigned integer to the buffer.
 void
 yp_buffer_append_u64(yp_buffer_t *buffer, uint64_t value);
 
+// Free the memory associated with the buffer and the buffer itself.
 void
 yp_buffer_free(yp_buffer_t *buffer);
 
