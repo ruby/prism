0b60840119947fd7259205caf3f1ec55e113a8f5
Kevin Newton <kddnewton@gmail.com>
2022-09-29 15:10:06 -0400

    Deserialization from Ruby

--- ext/yarp/buffer.h
+++ ext/yarp/buffer.h
@@ -1,6 +1,7 @@
 #ifndef YARP_BUFFER_H
 #define YARP_BUFFER_H
 
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
--- lib/yarp.rb
+++ lib/yarp.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "yarp/yarp"
+require_relative "yarp/serialization"
 require_relative "yarp/version"
 
 module YARP
