3d24e62f20c7eea92afc1cbf2fd8dce4e2891251
Kevin Newton <kddnewton@gmail.com>
2022-10-31 13:12:58 -0400

    Move magic comment lexing into its own file

--- src/yarp.c
+++ src/yarp.c
@@ -1074,6 +1074,32 @@ lex_token_type(yp_parser_t *parser) {
 /* Parse functions                                                            */
 /******************************************************************************/
 
+// Here we're going to check if this is a "magic" comment, and perform whatever
+// actions are necessary for it here.
+static void
+parser_lex_magic_comments(yp_parser_t *parser) {
+  const char *pointer = parser->current.start + 1;
+  while (char_is_non_newline_whitespace(pointer)) pointer++;
+
+  // There is a lot TODO here to make it more accurately reflect encoding
+  // parsing, but for now this gets us closer.
+  if (strncmp(pointer, "encoding:", 9) == 0) {
+    pointer += 9;
+    while (char_is_non_newline_whitespace(pointer)) pointer++;
+
+    if ((strncmp(pointer, "ascii", 5) == 0) || (strncmp(pointer, "us-ascii", 8) == 0)) {
+      parser->encoding.alnum_char = yp_encoding_ascii_alnum_char;
+      parser->encoding.alpha_char = yp_encoding_ascii_alpha_char;
+    } else if (strncmp(pointer, "utf-8", 5) == 0) {
+      parser->encoding.alnum_char = yp_encoding_utf8_alnum_char;
+      parser->encoding.alpha_char = yp_encoding_utf8_alpha_char;
+    } else {
+      // TODO: handling invalid encoding.
+      fprintf(stderr, "Could not parse encoding: %.*s\n", (int) (parser->current.end - pointer), pointer);
+    }
+  }
+}
+
 // Get the next token type and skip over comment tokens.
 static void
 parser_lex(yp_parser_t *parser) {
@@ -1091,33 +1117,10 @@ parser_lex(yp_parser_t *parser) {
     yp_comment_type_t type;
 
     switch (parser->current.type) {
-      case YP_TOKEN_COMMENT: {
-        // Here we're going to check if this is a "magic" comment, and perform
-        // whatever actions are necessary for it here.
-        const char *pointer = parser->current.start + 1;
-        while (char_is_non_newline_whitespace(pointer)) pointer++;
-
-        // There is a lot TODO here to make it more accurately reflect encoding
-        // parsing, but for now this gets us closer.
-        if (strncmp(pointer, "encoding:", 9) == 0) {
-          pointer += 9;
-          while (char_is_non_newline_whitespace(pointer)) pointer++;
-
-          if ((strncmp(pointer, "ascii", 5) == 0) || (strncmp(pointer, "us-ascii", 8) == 0)) {
-            parser->encoding.alnum_char = yp_encoding_ascii_alnum_char;
-            parser->encoding.alpha_char = yp_encoding_ascii_alpha_char;
-          } else if (strncmp(pointer, "utf-8", 5) == 0) {
-            parser->encoding.alnum_char = yp_encoding_utf8_alnum_char;
-            parser->encoding.alpha_char = yp_encoding_utf8_alpha_char;
-          } else {
-            // TODO: handling invalid encoding.
-            fprintf(stderr, "Could not parse encoding: %.*s\n", (int) (parser->current.end - pointer), pointer);
-          }
-        }
-
+      case YP_TOKEN_COMMENT:
+        parser_lex_magic_comments(parser);
         type = YP_COMMENT_INLINE;
         break;
-      }
       case YP_TOKEN___END__:
         type = YP_COMMENT___END__;
         break;
