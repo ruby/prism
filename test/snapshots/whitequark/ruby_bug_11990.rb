8148f237ff1e02c15c427770054d7d50d6b9990e
Kevin Newton <kddnewton@gmail.com>
2023-03-30 11:28:31 -0400

    Do not accidentally parse arguments twice on vcalls

--- test/snapshots/whitequark/ruby_bug_11990.rb
+++ test/snapshots/whitequark/ruby_bug_11990.rb
@@ -1,17 +1,30 @@
-ProgramNode(0...0)(
+ProgramNode(0...1)(
   ScopeNode(0...0)([]),
-  StatementsNode(0...0)(
-    [CallNode(0...0)(
+  StatementsNode(0...1)(
+    [CallNode(0...1)(
        nil,
        nil,
        IDENTIFIER(0...1)("p"),
        nil,
-       ArgumentsNode(7...12)(
-         [StringNode(7...12)(
-            STRING_BEGIN(7...8)("\""),
-            STRING_CONTENT(8...11)("  y"),
-            STRING_END(11...12)("\""),
-            "  y"
+       ArgumentsNode(13...12)(
+         [StringConcatNode(13...12)(
+            HeredocNode(13...17)(
+              HEREDOC_START(2...6)("<<~E"),
+              [StringNode(13...17)(
+                 nil,
+                 STRING_CONTENT(13...17)("  x\n"),
+                 nil,
+                 "x\n"
+               )],
+              HEREDOC_END(17...19)("E\n"),
+              2
+            ),
+            StringNode(7...12)(
+              STRING_BEGIN(7...8)("\""),
+              STRING_CONTENT(8...11)("  y"),
+              STRING_END(11...12)("\""),
+              "  y"
+            )
           )]
        ),
        nil,
