038ce3bed9f514d22df14ab1fef3b2870e8b6e3c
Kevin Newton <kddnewton@gmail.com>
2023-03-31 11:16:38 -0400

    Shared ASCII tables where possible

--- src/enc/euc_jp.c
+++ src/enc/euc_jp.c
@@ -2,33 +2,6 @@
 
 typedef uint32_t euc_jp_codepoint_t;
 
-#define EUC_JP_ALPHA_CODEPOINTS_LENGTH 4
-euc_jp_codepoint_t euc_jp_alpha_codepoints[EUC_JP_ALPHA_CODEPOINTS_LENGTH] = {
-  0x41, 0x5A,
-  0x61, 0x7A,
-};
-
-#define EUC_JP_ALNUM_CODEPOINTS_LENGTH 6
-euc_jp_codepoint_t euc_jp_alnum_codepoints[EUC_JP_ALNUM_CODEPOINTS_LENGTH] = {
-  0x30, 0x39,
-  0x41, 0x5A,
-  0x61, 0x7A,
-};
-
-#define EUC_JP_ISUPPER_CODEPOINTS_LENGTH 2
-euc_jp_codepoint_t euc_jp_isupper_codepoints[EUC_JP_ISUPPER_CODEPOINTS_LENGTH] = {
-  0x41, 0x5A,
-};
-
-static bool
-euc_jp_codepoint_match(euc_jp_codepoint_t codepoint, euc_jp_codepoint_t *codepoints, size_t size) {
-  for (size_t index = 0; index < size; index += 2) {
-    if (codepoint >= codepoints[index] && codepoint <= codepoints[index + 1])
-      return true;
-  }
-  return false;
-}
-
 static euc_jp_codepoint_t
 euc_jp_codepoint(const char *c, size_t *width) {
   const unsigned char *uc = (const unsigned char *) c;
@@ -65,7 +38,12 @@ yp_encoding_euc_jp_alpha_char(const char *c) {
   size_t width;
   euc_jp_codepoint_t codepoint = euc_jp_codepoint(c, &width);
 
-  return (codepoint && euc_jp_codepoint_match(codepoint, euc_jp_alpha_codepoints, EUC_JP_ALPHA_CODEPOINTS_LENGTH)) ? width : 0;
+  if (width == 1) {
+    const char value = (const char) codepoint;
+    return yp_encoding_ascii_alpha_char(&value);
+  } else {
+    return 0;
+  }
 }
 
 size_t
@@ -73,7 +51,12 @@ yp_encoding_euc_jp_alnum_char(const char *c) {
   size_t width;
   euc_jp_codepoint_t codepoint = euc_jp_codepoint(c, &width);
 
-  return (codepoint && euc_jp_codepoint_match(codepoint, euc_jp_alnum_codepoints, EUC_JP_ALNUM_CODEPOINTS_LENGTH)) ? width : 0;
+  if (width == 1) {
+    const char value = (const char) codepoint;
+    return yp_encoding_ascii_alnum_char(&value);
+  } else {
+    return 0;
+  }
 }
 
 bool
@@ -81,9 +64,10 @@ yp_encoding_euc_jp_isupper_char(const char *c) {
   size_t width;
   euc_jp_codepoint_t codepoint = euc_jp_codepoint(c, &width);
 
-  return codepoint && euc_jp_codepoint_match(codepoint, euc_jp_isupper_codepoints, EUC_JP_ISUPPER_CODEPOINTS_LENGTH);
+  if (width == 1) {
+    const char value = (const char) codepoint;
+    return yp_encoding_ascii_isupper_char(&value);
+  } else {
+    return 0;
+  }
 }
-
-#undef EUC_JP_ALPHA_CODEPOINTS_LENGTH
-#undef EUC_JP_ALNUM_CODEPOINTS_LENGTH
-#undef EUC_JP_ISUPPER_CODEPOINTS_LENGTH
