a82fb35743ea64c8791abd6ee16176024d874a1d
Jemma Issroff <jemmaissroff@gmail.com>
2023-06-06 14:55:04 -0400

    Added extra define strncasecmp

--- include/yarp/missing.h
+++ include/yarp/missing.h
@@ -16,7 +16,9 @@ const char * yp_strnstr(const char *haystack, const char *needle, size_t length)
 int yp_strncasecmp(const char *string1, const char *string2, size_t length);
 
 #ifndef HAVE_STRNCASECMP
+#ifndef strncasecmp
 #define strncasecmp yp_strncasecmp
+#endif
 #endif
 
 #endif
