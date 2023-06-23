4a6fd1c32b3ed5010fda80713c6dda692d143a3f
Steven Johnstone <steven.james.johnstone@gmail.com>
2023-06-23 21:30:30 +0100

    Fix yp_snprintf

--- src/util/yp_snprintf.c
+++ src/util/yp_snprintf.c
@@ -6,7 +6,7 @@ int
 yp_snprintf(char *dest, YP_ATTRIBUTE_UNUSED size_t size, const char *format, ...) {
     va_list args;
     va_start(args, format);
-    int result = sprintf(dest, format, args);
+    int result = vsprintf(dest, format, args);
     va_end(args);
     return result;
 }
