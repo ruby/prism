b1251f70efdeb37dedf102e33a2713b7b4313f7e
Kevin Newton <kddnewton@gmail.com>
2023-08-08 15:34:30 -0400

    Fix up merge conflict

--- test/parse_serialize_test.rb
+++ test/parse_serialize_test.rb
@@ -7,14 +7,14 @@ class ParseSerializeTest < Test::Unit::TestCase
     dumped = YARP.const_get(:Debug).parse_serialize_file(__FILE__)
     result = YARP.load(File.read(__FILE__), dumped)
 
-    assert_kind_of YARP::ProgramNode, result, "Expected the root node to be a ProgramNode"
+    assert_kind_of YARP::ParseResult, result, "Expected the return value to be a ParseResult"
     assert_equal __FILE__, find_file_node(result)&.filepath, "Expected the filepath to be set correctly"
   end
 
   private
 
-  def find_file_node(program)
-    queue = [program]
+  def find_file_node(result)
+    queue = [result.value]
 
     while (node = queue.shift)
       return node if node.is_a?(YARP::SourceFileNode)
