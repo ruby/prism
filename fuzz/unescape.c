36a5b801c440ddb7c08ac38f9430e08cf95be7d4
Kevin Newton <kddnewton@gmail.com>
2023-08-23 15:48:24 -0400

    Encoding-dependent escapes

--- fuzz/unescape.c
+++ fuzz/unescape.c
@@ -6,12 +6,10 @@ unescape(const char *input, size_t size, yp_unescape_type_t type) {
     yp_parser_init(&parser, input, size, "unescape.c");
 
     yp_string_t string = YP_EMPTY_STRING;
-    yp_list_t error_list = YP_LIST_EMPTY;
-    yp_unescape_manipulate_string(&parser, input, size, &string, type, &error_list);
+    yp_unescape_manipulate_string(&parser, &string, type);
 
     yp_parser_free(&parser);
     yp_string_free(&string);
-    yp_list_free(&error_list);
 }
 
 void
