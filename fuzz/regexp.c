c19b0fa185afd766599cb057b638459c031cf980
Kevin Newton <kddnewton@gmail.com>
2024-06-04 12:11:25 -0400

    Pass prism parser into regexp parser

--- fuzz/regexp.c
+++ fuzz/regexp.c
@@ -7,5 +7,10 @@ regexp_name_callback(const pm_string_t *name, void *data) {
 
 void
 harness(const uint8_t *input, size_t size) {
-    pm_regexp_parse(input, size, false, PM_ENCODING_UTF_8_ENTRY, regexp_name_callback, NULL);
+    pm_parser_t parser;
+    pm_parser_init(&parser, input, size, NULL);
+
+    pm_regexp_parse(&parser, input, size, regexp_name_callback, NULL);
+
+    pm_parser_free(&parser);
 }
