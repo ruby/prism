987e21d4adbee33e91fa4763c3b8517cd67907c1
Kevin Newton <kddnewton@gmail.com>
2024-06-05 10:50:18 -0400

    Report onigmo errors for depth

--- fuzz/regexp.c
+++ fuzz/regexp.c
@@ -6,11 +6,16 @@ regexp_name_callback(const pm_string_t *name, void *data) {
 }
 
 void
+regexp_error_callback(const uint8_t *start, const uint8_t *end, const char *message, void *data) {
+    // Do nothing
+}
+
+void
 harness(const uint8_t *input, size_t size) {
     pm_parser_t parser;
     pm_parser_init(&parser, input, size, NULL);
 
-    pm_regexp_parse(&parser, input, size, regexp_name_callback, NULL);
+    pm_regexp_parse(&parser, input, size, regexp_name_callback, NULL, regexp_error_callback, NULL);
 
     pm_parser_free(&parser);
 }
