74cbe5b252008adb5704a121309dd2ea4d4dcd45
Vinicius Stock <vinicius.stock@shopify.com>
2025-07-03 10:35:06 -0400

    Upgrade bindgen

--- rust/ruby-prism-sys/src/lib.rs
+++ rust/ruby-prism-sys/src/lib.rs
@@ -25,6 +25,8 @@
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
 #[allow(non_upper_case_globals)]
+#[allow(unused_qualifications)]
+#[allow(clippy::missing_const_for_fn)]
 mod bindings {
     // In `build.rs`, we use `bindgen` to generate bindings based on C headers
     // and `libprism`. Here is where we pull in those bindings and make
