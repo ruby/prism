1d10ae22c3eae42cbdf50017566f1868c78aca1b
andreatp <andrea.peruffo1982@gmail.com>
2026-01-28 10:39:04 +0000

    Cleanup and improvements

--- java-wasm/src/main/java-templates/org/prism/WasmResource.java
+++ java-wasm/src/main/java-templates/org/prism/WasmResource.java
@@ -1,7 +1,7 @@
 package org.prism;
 
 public final class WasmResource {
-    public static final String absoluteFile = "file://${project.basedir}/src/main/resources/prism.wasm";
+    public static final String absoluteFile = "file://${project.basedir}/src/test/resources/prism.wasm";
 
     private WasmResource() {}
 }
