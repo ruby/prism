d3153258af99d8db971445a3c083f3056d365f79
Kevin Newton <kddnewton@gmail.com>
2026-03-17 20:04:20 -0400

    Give full lifetime functions to parser

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -6,19 +6,18 @@ extern "C" {
 
 int main() {
     pm_arena_t arena = { 0 };
-    pm_parser_t parser;
-    pm_parser_init(&arena, &parser, reinterpret_cast<const uint8_t *>("1 + 2"), 5, NULL);
+    pm_parser_t *parser = pm_parser_new(&arena, reinterpret_cast<const uint8_t *>("1 + 2"), 5, NULL);
 
-    pm_node_t *root = pm_parse(&parser);
+    pm_node_t *root = pm_parse(parser);
     pm_buffer_t *buffer = pm_buffer_new();
 
-    pm_prettyprint(buffer, &parser, root);
+    pm_prettyprint(buffer, parser, root);
 
     std::string_view view(pm_buffer_value(buffer), pm_buffer_length(buffer));
     std::cout << view << std::endl;
 
     pm_buffer_free(buffer);
-    pm_parser_cleanup(&parser);
+    pm_parser_free(parser);
     pm_arena_free(&arena);
 
     return 0;
