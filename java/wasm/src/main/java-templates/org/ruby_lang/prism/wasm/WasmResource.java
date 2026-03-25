91a65903c635099378aabe7da7be0fba8ee27a27
andreatp <andrea.peruffo1982@gmail.com>
2026-03-25 12:42:47 +0000

    [chore] Improve Chicory usage DX

--- java/wasm/src/main/java-templates/org/ruby_lang/prism/wasm/WasmResource.java
+++
@@ -1,7 +0,0 @@
-package org.ruby_lang.prism.wasm;
-
-public final class WasmResource {
-    public static final String absoluteFile = "file://${project.basedir}/src/test/resources/prism.wasm";
-
-    private WasmResource() {}
-}
